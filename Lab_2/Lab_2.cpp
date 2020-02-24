6a1422224f2470154d69b4dbe9889d9a388334c0
RistalTV <ristalvtgames@yandex.com>
2020-02-25 01:28:11 +0300

    Create lab 2

---
+++ Lab_2/Lab_2.cpp
@@ -0,0 +1,37 @@
+﻿// Lab_2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <vector>
+#include <ctime>
+#include <cstdlib>
+#include <clocale>
+
+using namespace std;
+
+
+int main()
+{
+	setlocale(0, "");
+	srand((unsigned int)time(NULL));
+	unsigned int const CountElem = rand() % 80 + 12;
+	cout << "Количество элементов вектора: " << CountElem << "\n" << "Вектор: ";
+	vector<int> vec1(CountElem);
+//
+//	Задаём значение элементам вектора.Вывод вектора
+//
+	for (unsigned int i = 0; i < CountElem; i++)
+	{
+		vec1[i] = rand() % 200 - 100;
+		cout << vec1[i] << " ";
+	}
+	cout << endl;
+//
+//	Сортировка пузырьковым методом
+//
+	for (unsigned int i = 0; i < CountElem; i++)
+	{
+
+	}
+	return 0;
+}
