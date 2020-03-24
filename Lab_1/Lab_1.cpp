9d500ab1c9fccf2ced7cb86beade2ce2c9994625
Ristal <ristaltvgames@gmail.com>
2020-03-25 02:04:20 +0300

    Revert "update lab 1 v 2.0"
    This reverts commit 79d82c7a3a26b4f3ff255c0b3ebd4ce470a0fb55.

--- Lab_1/Lab_1.cpp
+++ Lab_1/Lab_1.cpp
@@ -11,127 +11,107 @@ using namespace std;
 
 bool func_parity(int CountElem);
 int func_MiddleElem(int CountElement);
-int func_scan(int CountElem);
-//
-//	Главная функция
-//
+int func_scan(void);
 int main()
 {
 	setlocale(0, "");
 	srand((unsigned int)time(NULL));
-	unsigned int const CountElement = rand() %80+12;
+	unsigned int const CountElement = rand() % 80+12;
 	unsigned int const MAX = 100;
-	unsigned int I;
-	unsigned int j = CountElement - 1;
-	unsigned int editCountElement = CountElement - 1;
-	unsigned int maxCountElem = editCountElement + 3;
-	unsigned int MaxInputElem = editCountElement - 10;
 	array <int, MAX> ArrMain = {};
+	array <int, MAX> ArrCopy = {};
 	cout << "Количество элементов массива: " << CountElement << "\n" <<"Массив: ";
-	//	Задаём значение элементам массива.Вывод массива
-	{
+//
+//	Задаём значение элементам массива.Вывод массива
+//
 	for (unsigned int i = 0; i < CountElement; i++)
 	{
-		ArrMain[i] = rand() % 200 - 100;
+		ArrMain[i] = rand() % 200-100;
+		cout << ArrMain[i] << " ";
+	}
+
+	cout << endl;
+//
+//  Переворачиваем массив
+//
+	unsigned int j = CountElement - 1;
+	for (unsigned int i = 0; i < CountElement; i++)
+	{
+		//cout << "i = " << i << " - ArrCopy[i] = " << ArrCopy[i] << "\n" << "j = " << j << " - ArrMain[j] = " << ArrMain[j] << "\n";
+		ArrCopy[i] = ArrMain[j];
+		ArrMain[j] = 0;
+		//cout << "\n\ni = " << i << " - ArrCopy[i] = " << ArrCopy[i] << "\n=============================================================\n\n";
+		j--;
+	}
+//
+//	Вывод перевёрнутого массива
+//
+	cout << "Вывод перевёрнутого массива: ";
+	for (unsigned int i = 0; i < CountElement; i++)
+	{
+		cout << ArrCopy[i]<< " ";
+	}
+	cout << endl;
+//
+//	Дествия после проверки на чётность(если нечётное то не записываем ср.число)
+//
+	if (func_parity(CountElement))
+	{
+		for (unsigned int i = 0; i < CountElement; i++)
+		{
+			ArrMain[i] = ArrCopy[i];
+		}
+	}
+	else 
+	{
+		unsigned int MiddleElem = func_MiddleElem(CountElement);
+		unsigned int j = MiddleElem + 1;
+		for (unsigned int i = 0; i < MiddleElem; i++)
+		{
+			ArrMain[i] = ArrCopy[i];
+		}
+		for (unsigned int i = MiddleElem; i < CountElement; i++)
+		{
+			ArrMain[i] = ArrCopy[j];
+			j++;
+		}
+		cout << "Удаляемый элемент:" << ArrCopy[MiddleElem] << endl;
+	}
+//
+//	Вывод массива после проверки на чётность
+//
+	unsigned int editCountElement= CountElement-1;
+	cout << "Массив после проверки на чётность: ";
+	for (unsigned int i = 0; i < editCountElement; i++)
+	{
 		cout << ArrMain[i] << " ";
 	}
 	cout << endl;
+//
+//  Добавление в массив 3 лемента со значением M[I+10]
+//
+
+	unsigned int I;
+	unsigned int maxCountElem = editCountElement + 3;
+	unsigned int MaxInputElem = editCountElement - 10;
+	for (unsigned int i = editCountElement; i < maxCountElem; i++)
+	{	
+		I = func_scan();
+		ArrMain[i] = ArrMain[I + 10] - 2;
 	}
-	//  Переворачиваем массив
-	{
-		int tempElem;
-		if (func_parity(CountElement)) 
-		{
-			unsigned int j = CountElement-1;
-			unsigned int MiddleElem = func_MiddleElem(CountElement) + 1;
-			for (unsigned int i = 0; i < MiddleElem; i++)
-			{
-				tempElem = ArrMain[j];
-				ArrMain[j] = ArrMain[i];
-				ArrMain[i] = tempElem;
-				j--;
-			}
-		}
-		else
-		{
-			unsigned int MiddleElem = func_MiddleElem(CountElement) + 1;
-			unsigned int j = CountElement - 1;
-			for (unsigned int i = 0; i < MiddleElem; i++)
-			{
-				tempElem = ArrMain[j];
-				ArrMain[j] = ArrMain[i];
-				ArrMain[i] = tempElem;
-				j--;
-			}
-		}
-	}
-	//	Вывод перевёрнутого массива
-	{
-		cout << "Вывод перевёрнутого массива: ";
-		for (unsigned int i = 0; i < CountElement; i++)
-		{
-			cout << ArrMain[i] << " ";
-		}
-		cout << endl;
-	}
-	//	Дествия после проверки на чётность(если нечётное то не записываем ср.число)
-	{
-		if (!func_parity(CountElement))
-		{
-			unsigned int MiddleElem = func_MiddleElem(CountElement);
-			cout << "Удаляемый элемент:" << ArrMain[MiddleElem] << endl;
-			for (unsigned int i = MiddleElem; i < CountElement; i++)
-			{
-				ArrMain[i] = ArrMain[i+1];
-			}
-		
-			//	Вывод массива после проверки на чётность
-			{
-				cout << "Массив после проверки на чётность: ";
-				for (unsigned int i = 0; i < editCountElement; i++)
-				{
-					cout << ArrMain[i] << " ";
-				}
-				cout << endl;
-			}
-		}
-		else
-		{
-			//	Вывод массива после проверки на чётность
-			{
-				cout << "Массив после проверки на чётность: ";
-				for (unsigned int i = 0; i < editCountElement+1; i++)
-				{
-					cout << ArrMain[i] << " ";
-				}
-				cout << endl;
-			}
-		}
-	}
-	//  Добавление в массив 3 лемента со значением M[I+10]
-	{
-		for (unsigned int i = editCountElement; i < maxCountElem; i++)
-		{
-			I = func_scan(CountElement);
-			ArrMain[i] = ArrMain[I + 10] - 2;
-		}
-	}
-	//	Вывод коннечного массива
-	{
+//
+//	Вывод коннечного массив
+//
 	cout << "                                   Результат\n" << "________________________________________________________________________________";
 	cout << "\nМассив после проверки на чётность: ";
 	for (unsigned int i = 0; i < maxCountElem; i++)
 	{
 		cout << ArrMain[i] << " ";
 	}
-	cout << endl << "________________________________________________________________________________" << endl;
-	}
-	//	Конец программы
+	cout <<endl << "________________________________________________________________________________" << endl;
 	system("pause");
-	return 0;
+return 0;
 }
-
-
 //
 //	Проверка на чётность
 //
@@ -153,25 +133,25 @@ int func_MiddleElem(int CountElement)
 // 
 //	Функция считывания I с проверкой ввода с клавиатуры
 //
-int func_scan(int CountElem)
+int func_scan() 
 {
 	int I= -842234;
-		while ((I<0) or (I> CountElem))
+		while ((I<0) or (I>90))
 		{
 			if (I == -842234)
 				cout << "Введите значение I: ";
-			else if ((I < 0) or (I > CountElem))
+			else if ((I < 0) or (I > 90))
 				cout << "Введите верное значение I: ";
-			else if ((I > 0) or (I < CountElem))
+			else if ((I > 0) or (I < 90))
 				cout << "Введите значение I: ";
 			cin >> I;
 			if (cin.fail())
 			{
 				cout << "Введите верное значение I: ";
 				cin.clear();
-				cin.ignore(CountElem, '\n');
+				cin.ignore(100, '\n');
 			}
-			cin.ignore(CountElem, '\n');
+			cin.ignore(100, '\n');
 		}
 
 	return I;
